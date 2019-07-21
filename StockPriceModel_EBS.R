f5fb37641c76775eeada1f5015c85d157e6cfbaa
ClementeAsr <52258679+ClementeAsr@users.noreply.github.com>
2019-07-21 21:51:53 +0200

    Add files via upload

--- StockPriceModel_EBS.R
+++ StockPriceModel_EBS.R
@@ -1,12 +1,20 @@
 #Financial data from Yahoo! Finance - also refer to finance.yahoo.com/lookup
 
 #############################################################
-#############      Parameter definition:    #################
+#############      Necessary packages:      #################
 list.of.packages <- c("quantmod", "ggplot2","magrittr","forecast","lubridate","tseries")
-ts_sampling_per <- 'weekly'
+#############################################################
+
+#############################################################
+#############      Parameter definition:    #################
+p.value <- 0.05
+train.test.split <- 0.8
+ts_sampling_per <- 'monthly'
 ts_sampling_meth <- 'mean'
 start_date <- as.Date("2009-12-24")
 end_date <- as.Date(today(tzone="GMT"))
+transformation <- 1
+forecast_error_hor <- 3
 #############################################################
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -38,44 +46,55 @@ switch(ts_sampling_per,
        daily = GDAXI_close <-GDAXI_close,
        monthly = GDAXI_close <-apply.monthly(GDAXI_close, ts_sampling_meth))
 
-#Divide data in train and test set --> as usual with Time Series, the chronologically first 80% of data points is the train set
-GDAXI_close.end <- floor(0.8*length(GDAXI_close)) #select the first 80% of the data
-GDAXI_close.train <- GDAXI_close[1:GDAXI_close.end,] #assign the first 80% of the data to the train set
-GDAXI_close.test <- GDAXI_close[(GDAXI_close.end+1):length(GDAXI_close),] #assign the most recent 20% to the test set
-
-#Check if the data separation went well - if not, interrupt
-test <- length(GDAXI_close) - (length(GDAXI_close.test)+length(GDAXI_close.train))
-try(if(test != 0) stop("Train and test set do not make the total set"))
-
 #Convert the xts object to the ts class:
-
+DAX.end <- floor(train.test.split*length(GDAXI_close)) #select the first x% of the data
 switch(ts_sampling_per,
-       weekly = pred <- c(freq = 52, start.train=c(year(start(GDAXI_close.train)),week(start(GDAXI_close.train))), start.test=c(year(start(GDAXI_close.test)),week(start(GDAXI_close.test)))),
-       daily =  pred <- c(freq = 365, start.train=c(year(start(GDAXI_close.train)),day(start(GDAXI_close.train))), start.test=c(year(start(GDAXI_close.test)),day(start(GDAXI_close.test)))),
-       monthly = pred <- c(freq = 12, start.train=c(year(start(GDAXI_close.train)),month(start(GDAXI_close.train))), start.test=c(year(start(GDAXI_close.test)),month(start(GDAXI_close.test)))))
+       weekly = pred <- c(freq = 52, start.train=c(year(start(GDAXI_close)),week(start(GDAXI_close))), start.test=c(year(start(GDAXI_close[DAX.end+1,])),week(start(GDAXI_close[DAX.end+1,])))),
+       daily =  pred <- c(freq = 365, start.train=c(year(start(GDAXI_close)),day(start(GDAXI_close))), start.test=c(year(start(GDAXI_close[DAX.end+1,])),day(start(GDAXI_close[DAX.end+1,])))),
+       monthly = pred <- c(freq = 12, start.train=c(year(start(GDAXI_close)),month(start(GDAXI_close))), start.test=c(year(start(GDAXI_close[DAX.end+1,])),month(start(GDAXI_close[DAX.end+1,])))))
 
 DAX <- ts(as.numeric(GDAXI_close),start=c(pred[2],pred[3]),frequency = pred[1])
-DAX.train <-ts(as.numeric(GDAXI_close.train), start = c(pred[2],pred[3]), frequency = pred[1])
-DAX.test <-ts(as.numeric(GDAXI_close.test), start = c(pred[4],pred[5]), frequency = pred[1])
-
-#Check if conversion went well
-try(if(class(DAX.train) != "ts" || class(DAX.test) != "ts") stop("Conversion into Time Series failed"))
-try(if(end(DAX.test)[1] != year(today()) || end(DAX.test)[2] != week(today())) stop("Date of object ts not consistent") )
-
-DAX.train.components <- decompose(DAX.train)
-plot(DAX.train.components)
-
+try(if(class(DAX) != "ts") stop("Conversion into Time Series failed"))
 
 ##Test formally for stationarity
-adf_test_trend <- tseries::adf.test(DAX,alternative="stationary") #Check if there is a trend in the TS
-adf_test_vola <- tseries::adf.test(DAX,alternative="explosive") #Check if there is a change in sd over time in TS
+adf.test.stat <- tseries::adf.test(DAX,alternative="stationary") #Check if there is a trend in the TS
 
-##Make the TS stationary
-## tbd
+if(adf.test.stat$p.value > p.value && transformation){
+  DAX <- log(DAX)
+  DAX <- diff(DAX, lag = 1)
+  plot(DAX)
+  adf.test.stat.reform <- tseries::adf.test(DAX,alternative="stationary") #Check if TS is stationary
+}
+try(if(adf.test.stat.reform$p.value > p.value) stop("Time Series couldn't be made stationary"))
 
-#DAX.train%>% 
- #HoltWinters(beta = TRUE, gamma = TRUE) %>% 
-  #forecast(h=length(DAX.test)) %>% 
-  #plot()
-#lines(DAX.test, col = "red")
+#Split time series in train and test set --> as usual with Time Series, the chronologically first x% of data points is the train set
+DAX.train <- ts(DAX[1:DAX.end], start = c(pred[2],pred[3]), frequency = pred[1]) #assign the first x% of the data to the train set
+DAX.test <- ts(DAX[(DAX.end+1):length(DAX)], start = c(pred[4],pred[5]), frequency = pred[1]) #assign the most recent (1-x)% to the test set
 
+#Check if the data separation went well - if not, interrupt
+test <- length(DAX) - (length(DAX.test)+length(DAX.train))
+try(if(test != 0) stop("Train and test set do not make the total set"))
+try(if(end(DAX.test)[1] != year(today()) || end(DAX.test)[2] != week(today())) stop("Date of object ts not consistent") )
+
+#Holt-Winter forecast
+forecast.HW <- HoltWinters(DAX.train,alpha=TRUE, beta = FALSE, gamma = TRUE, seasonal = "additive") 
+forecast.DAX <- forecast(forecast.HW,h=length(DAX.test))
+plot(forecast.DAX,ylim=c(-0.5,0.5))
+lines(DAX.test, col = "red")
+hw.mse <- 1/forecast_error_hor * sum((DAX.test[1:forecast_error_hor]-forecast.DAX$mean[1:forecast_error_hor])^2)
+
+#ARIMA forecast
+forecast.model.arima <- auto.arima(DAX.train)
+forecast.DAX <- forecast(forecast.model.arima,h=length(DAX.test))
+plot(forecast.DAX)#,ylim=c(-0.5,0.5))
+lines(DAX.test, col = "red")
+arima.mse <- 1/forecast_error_hor * sum((DAX.test[1:forecast_error_hor]-forecast.DAX$mean[1:forecast_error_hor])^2)
+
+#ets forecast
+forecast.expsmoothingsstate <- ets(DAX.train,model="ANA",opt.crit = "mse")
+forecast.DAX <- forecast(forecast.expsmoothingsstate,h=length(DAX.test))
+plot(forecast.DAX)#,ylim=c(-0.5,0.5))
+lines(DAX.test, col = "red")
+ets.mse <- 1/forecast_error_hor * sum((DAX.test[1:forecast_error_hor]-forecast.DAX$mean[1:forecast_error_hor])^2)
+
+mse.res <- c(hw.mse,arima.mse,ets.mse)
