6b741e5776bd5a88fc236cb9b1bd4f0aad4c5df0
r-Files <33875788+r-Files@users.noreply.github.com>
2019-08-07 16:38:22 +0200

    Update StockPriceModel_EBS.R
    function today() can only be called after loading the package lubridate

--- StockPriceModel_EBS.R
+++ StockPriceModel_EBS.R
@@ -5,6 +5,16 @@
 list.of.packages <- c("quantmod", "ggplot2","magrittr","forecast","lubridate","tseries")
 #############################################################
 
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library('quantmod')
+library('ggplot2')
+library('magrittr')
+library('forecast')
+library('lubridate')
+library('tseries')
+
 #############################################################
 #############      Parameter definition:    #################
 p.value <- 0.05
@@ -15,17 +25,8 @@ start_date <- as.Date("2009-12-24")
 end_date <- as.Date(today(tzone="GMT"))
 transformation <- 1
 forecast_error_hor <- 3
+
 #############################################################
-
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
-library('quantmod')
-library('ggplot2')
-library('magrittr')
-library('forecast')
-library('lubridate')
-library('tseries')
 
 #Get the index value of the DAX (=performance index) from Yahoo Finance
 #The object 'GDAXI' will be of type xts and contain 6 columns (5 price types + volume)
